89a4c41a9e8250b0b390455a6aad29ebc0b452e4
unknown <theodoreonyejiaku@gmail.com>
2020-11-05 04:11:14 +0100

    push to heroku and connect with github

--- routers/adminRouter.js
+++ routers/adminRouter.js
@@ -3,9 +3,9 @@ const adminRouter = express.Router();
 const adminController = require("../controllers/userController");
 const verification = require("../validation/validateToken");
 
-adminRouter.get("/admin/register", adminController.registerAdmin);
+// adminRouter.get("/admin/register", adminController.registerAdmin);
 
-adminRouter.get("/admin/login", adminController.get_signin);
+// adminRouter.get("/admin/login", adminController.get_signin);
 
 
 module.exports = adminRouter;
